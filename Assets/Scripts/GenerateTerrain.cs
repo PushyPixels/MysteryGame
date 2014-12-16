6171c27b1216a93e34e67df8458fb296547065b6
Maximilian Maroe <drakfyre@gmail.com>
2014-12-15 21:43:00 -0800

    Snowloucaust The Movie

---
+++ Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateTerrain : MonoBehaviour
+{
+	public float perlinFactor = 100.0f;
+	private Terrain terrain;
+		
+	[ContextMenu("GeneratePerlinTerrain")]
+	void GeneratePerlinTerrain()
+	{
+		terrain = GetComponent<Terrain>();
+
+		float[,] heights = new float[terrain.terrainData.heightmapWidth,terrain.terrainData.heightmapHeight];
+
+		float xOffset = Random.Range(0.0f,1000.0f);
+		float yOffset = Random.Range(0.0f,1000.0f);
+
+		for(int x = 0; x < terrain.terrainData.heightmapWidth; x++)
+		{
+			for(int y = 0; y < terrain.terrainData.heightmapHeight; y++)
+			{
+				heights[x,y] = Mathf.PerlinNoise(x/perlinFactor + xOffset,y/perlinFactor + yOffset);
+			}
+		}
+
+
+		terrain.terrainData.SetHeights(0,0,heights);
+	}
+}
---
+++ Assets/Scripts/GibOnCollide.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnCollide : MonoBehaviour
+{
+	public GameObject[] gibs;
+	[Tooltip("Object to destroy when colliding (Optional)")]
+	public GameObject destroyTarget;
+	public bool onCollide = true;
+	public bool onTrigger = true;
+
+	private GameObject target;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if(destroyTarget != null)
+		{
+			target = destroyTarget;
+		}
+		else
+		{
+			target = gameObject;
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		if(onCollide)
+		{
+			Gib();
+		}
+	}
+
+	void OnTriggerEnter()
+	{
+		if(onTrigger)
+		{
+			Gib();
+		}
+	}
+	
+
+	void Gib ()
+	{
+		foreach(GameObject gib in gibs)
+		{
+			Instantiate (gib,transform.position,transform.rotation);
+		}
+		Destroy(target);
+	}
+}
