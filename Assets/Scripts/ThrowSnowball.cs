30c8f4a46cee475310a7e593eb794bacf1f26734
Maximilian Maroe <drakfyre@gmail.com>
2014-12-22 21:29:24 -0800

    Snowball throwing logic.  Some broken stuff due to not using the third person camera script.

---
+++ Assets/Scripts/ThrowSnowball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowSnowball : MonoBehaviour
+{
+	private Animator throwingArm;
+	public float force = 10.0f;
+	public ForceMode forceMode = ForceMode.VelocityChange;
+	public string throwButton = "Fire1";
+	public string throwAnimationTriggerName = "ThrowSnowball";
+	public Rigidbody snowballPrefab;
+	public Transform throwPosition;
+	public Transform throwAngle;
+
+	void Start()
+	{
+		throwingArm = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetButtonDown(throwButton))
+		{
+			throwingArm.SetTrigger(throwAnimationTriggerName);
+		}
+	}
+
+	void ThrowEvent()
+	{
+		Rigidbody instance = Instantiate(snowballPrefab,throwPosition.position,throwAngle.rotation) as Rigidbody;
+		instance.AddForce(instance.transform.forward*force,forceMode);
+	}
+}
